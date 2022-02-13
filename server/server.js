12d0ad5a7c33d577d55499c4e10e9370331e2304
James Petran <jm.ptrn@gmail.com>
2022-02-12 22:31:06 -0600

    moved secure api to backend;

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const passport = require('./strategies/user.strategy');
 const userRouter = require('./routes/user.router');
 const satelliteRouter = require('./routes/satellite.router.js');
 const favoritesRouter = require('./routes/favorites.router.js');
+const threePapiRouter = require('./routes/threePapi.router.js')
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -25,8 +26,10 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
-app.use('/api/satellite', satelliteRouter)
-app.use('/api/favorites', favoritesRouter)
+app.use('/api/satellite', satelliteRouter);
+app.use('/api/favorites', favoritesRouter);
+app.use('/api/threePapi', threePapiRouter);
+
 
 // Serve static files
 app.use(express.static('build'));
--- src/redux/sagas/location.saga.js
+++ src/redux/sagas/location.saga.js
@@ -3,22 +3,19 @@ import { put, takeLatest } from 'redux-saga/effects';
 
 function* queryLocation(action) {
   console.log('in GET location from address')
-  const key = "AIzaSyACIq-9fmIlCVh1PfxrqpX1cnSEHs5fFek";
-  // console.log('key=',key);
   const address = encodeURI(action.payload);
-  // console.log(action.payload, "becomes", address);
-  const googleResult = yield axios.get(`
-  https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${key}
-  `);
+  const googleResult = yield axios.post('/api/threePapi/',{address});
   console.log(googleResult);
   let lat, lng, formattedAddress;
 
   if (googleResult.data.status === "ZERO_RESULTS") {
-    lat, lng, formattedAddress = null;
+    formattedAddress = null;
   } else {
-    lat = googleResult.data.results[0].geometry.location.lat;
-    lng = googleResult.data.results[0].geometry.location.lng;
-    formattedAddress = googleResult.data.results[0].formatted_address;
+    lat = googleResult.data.geometry.location.lat;
+    lng = googleResult.data.geometry.location.lng;
+    formattedAddress = googleResult.data.formatted_address;
+
+    console.log(lat, lng, formattedAddress)
   };
 
   console.log("result is:", lat, lng);
